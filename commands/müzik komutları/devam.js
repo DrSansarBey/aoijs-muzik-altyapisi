dfafac85fabb0f7d2bf2f20e0542b009c4a8b91f
Dr.SansarBey <74427616+DrSansarBey@users.noreply.github.com>
2021-07-04 13:22:18 +0300

    Create devam.js

---
+++ commands/müzik komutları/devam.js
@@ -0,0 +1,6 @@
+module.exports = ({
+name:"devam",
+aliases:"resume",
+code:`Durdurulan şarkı başarı ile **yeniden başlatıldı**.
+$resumeSong
+$suppressErrors[{title:HATA}{description:Herhangi bir şarkı çalmıyor.}{color:BLACK}]`})
