a448cd4fed7df42193302236799e1e91b40d1d79
Dr.SansarBey <74427616+DrSansarBey@users.noreply.github.com>
2021-07-04 13:25:59 +0300

    Create tekrarla.js

---
+++ commands/müzik komutları/tekrarla.js
@@ -0,0 +1,5 @@
+module.exports = ({
+name:"tekrarla",
+aliases:"loop",
+code:`Liste tekrarlaması şuan $replaceText[$replaceText[$loopQueue;true;**Aktif**!;1];false;**Deaktif**!;1]
+$suppressErrors[{title:HATA}{description:Herhangi bir şarkı çalmıyor.}{color:BLACK}]`})
