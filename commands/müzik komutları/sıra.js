2acede247530294479efaadf055a07af5536ced4
Dr.SansarBey <74427616+DrSansarBey@users.noreply.github.com>
2021-07-04 13:25:33 +0300

    Create sıra.js

---
+++ commands/müzik komutları/sıra.js
@@ -0,0 +1,7 @@
+module.exports = ({
+name:"sıra",
+aliases:"queue",
+code:`$title[Şarkı sırası]
+$description[$queue]
+$color[BLACK]
+$suppressErrors[{title:Şarkı Sırası}{description:Herhangi bir şarkı çalmıyor.}{color:BLACK}]`})
