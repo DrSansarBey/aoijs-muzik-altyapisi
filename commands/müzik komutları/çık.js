763342f87673f97763fd5110282acd4c24a94bb3
Dr.SansarBey <74427616+DrSansarBey@users.noreply.github.com>
2021-07-04 13:18:49 +0300

    Create çık.js

---
+++ commands/müzik komutları/çık.js
@@ -0,0 +1,6 @@
+module.exports = ({
+name:"çık",
+aliases:["leave","dc"],
+code:`$addCmdReactions[✅]
+$stopSong
+$suppressErrors[{title:HATA}{description:Herhangi bir şarkı çalmıyor.}{color:BLACK}]`})
