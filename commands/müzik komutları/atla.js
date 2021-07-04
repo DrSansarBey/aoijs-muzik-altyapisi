38b53436469061cf88a9bd3cdded5241f2596b58
Dr.SansarBey <74427616+DrSansarBey@users.noreply.github.com>
2021-07-04 13:17:10 +0300

    Create atla.js

---
+++ commands/müzik komutları/atla.js
@@ -0,0 +1,6 @@
+module.exports = ({
+name:"atla",
+aliases:["skip","s"],
+code:`Şarkı başarı ile **atlandı**.
+$skipSong
+$suppressErrors[{title:HATA}{description:Herhangi bir şarkı çalmıyor.}{color:BLACK}]`})
