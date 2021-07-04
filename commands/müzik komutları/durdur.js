d55e2308ec49ab84c34be12f9c08d43b2e88055f
Dr.SansarBey <74427616+DrSansarBey@users.noreply.github.com>
2021-07-04 13:22:50 +0300

    Create durdur.js

---
+++ commands/müzik komutları/durdur.js
@@ -0,0 +1,6 @@
+module.exports = ({
+name:"durdur",
+aliases:"stop",
+code:`Çalan şarkı başarı ile **durduruldu**.
+$pauseSong
+$suppressErrors[{title:HATA}{description:Herhangi bir şarkı çalmıyor.}{color:BLACK}]`})
